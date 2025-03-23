7b4203f27f603d8bd5c98c8b70ea5ad5fb3b37a8
Anderson-Portes <portsanderson@gmail.com>
2025-03-23 15:31:49 -0300

    changing database to mongodb

--- prisma/migrations/20250322015603_create_task_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "description" TEXT NOT NULL,
-    "startTime" DATETIME NOT NULL,
-    "endTime" DATETIME,
-    "userId" TEXT NOT NULL,
-    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
